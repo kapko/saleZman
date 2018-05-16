b70c37540835906a35208b4d5bea1d5419680035
kzhekshen <kzhekshen@incentient.com>
2018-05-16 11:43:04 +0600

    fixed

--- src/components/search-list/search-list.ts
+++ src/components/search-list/search-list.ts
@@ -94,7 +94,7 @@ export class SearchListComponent {
     if (this.lastKey === key) {
       this.showScroll = false;
     }
-  
+
     key = this.lastKey;
     this.getData(this.lastKey, true);
   }
